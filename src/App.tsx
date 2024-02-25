9efb542b8b46adbb63820a1718c06ff77114b7b0
Hallelujah78 <gavan.browne@gmail.com>
2024-02-25 01:09:12 +0000

    App.tsx updates.
    Added state and handlers for enter key, delete key, and alpha key. Possible memory leak and may not be adding/removing event listener in useEffect correctly. Tis late.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,68 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { getRandomArbitrary } from "./utils/utils";
 import { words } from "./data/data.ts";
 
-type WurdilGuess = [string, string, string, string, string];
+type WurdilGuess = string[];
 
 const App: React.FC = () => {
-  const [currentGuessNumber, setCurrentGuessNumber] = useState(1);
-  const [positionBlankLetter, setPositionBlankLetter] = useState<number>(0);
-  const [answer, setAnswer] = useState<string>("");
-  const [currentGuessValue, setCurrentGuessValue] = useState<WurdilGuess>([
-    "",
-    "",
-    "",
-    "",
-    "",
+  const [guesses, setGuesses] = useState<
+    [
+      WurdilGuess,
+      WurdilGuess,
+      WurdilGuess,
+      WurdilGuess,
+      WurdilGuess,
+      WurdilGuess
+    ]
+  >([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
   ]);
+  const [currentGuessIndex, setCurrentGuessIndex] = useState<number>(0);
+  const [currentLetterIndex, setCurrentLetterIndex] = useState<number>(0);
+  const [answer, setAnswer] = useState<string>("");
 
-  const enterKeyHandler = () => {};
+  const enterKeyHandler = () => {
+    // is guess a valid word?
+    // then
+  };
   const deleteKeyHandler = () => {};
-  const alphaKeyPressHandler = () => {
-    const currentGuessNumberDOM = Array.from(document.querySelectorAll(
-      `guess-${currentGuessNumber}`)
-    const positionBlankLetterDOM = currentGuessNumberDOM.find((span)=>{
-      +span.id === positionBlankLetter;
-    })
-    );
+  const alphaKeypressHandler = useCallback(() => {
     // a key is pressed
     // under certain circumstances we do nothing
     // positionBlankLetter is between 0-4 inclusive and the key is a-z
     // then set value of the appropriate span to that letter and increment positionBlankLetter
-    setCurrentGuessValue((prev) => [...prev], "a");
-  };
+    console.log(currentLetterIndex);
+    console.log(guesses[0]);
+
+    setGuesses((prev) => {
+      prev[currentGuessIndex][currentLetterIndex] = "A";
+      return prev;
+    });
+    setCurrentLetterIndex((prev) => {
+      if (prev < 4) {
+        prev = prev + 1;
+      }
+      return prev;
+    });
+  }, [currentGuessIndex, currentLetterIndex, guesses]);
 
   const startGame = () => {
     setAnswer(words[getRandomArbitrary(0, words.length - 1)]);
   };
 
   useEffect(() => {
-    document.addEventListener("keyup", alphaKeyPressHandler);
+    document.addEventListener("keyup", alphaKeypressHandler);
 
     return () => {
-      document.removeEventListener("keyup", alphaKeyPressHandler);
+      document.removeEventListener("keyup", alphaKeypressHandler);
     };
-  }, []);
+  }, [alphaKeypressHandler]);
 
   return (
     <Wrapper>
@@ -53,13 +72,30 @@ const App: React.FC = () => {
       </nav>
       <section>
         <input type="text" />
-        <div id="guess-1">
+        <div id="guess-0">
+          <div className="letter" id="0">
+            {guesses[0][0]}
+          </div>
+          <div className="letter" id="1">
+            {guesses[0][1]}
+          </div>
+          <div className="letter" id="2">
+            {guesses[0][2]}
+          </div>
+          <div className="letter" id="3">
+            {guesses[0][3]}
+          </div>
+          <div className="letter" id="4">
+            {guesses[0][4]}
+          </div>
+        </div>
+        {/* <div id="guess-1">
           <span id="0"></span>
           <span id="1"></span>
           <span id="2"></span>
           <span id="3"></span>
           <span id="4"></span>
-        </div>
+        </div> */}
       </section>
       <h3>{answer}</h3>
     </Wrapper>
@@ -86,18 +122,22 @@ const Wrapper = styled.div`
     display: grid;
     place-content: center;
     max-width: 100%;
-
-    input {
-      height: 3rem;
-      text-align: center;
-      font-size: 3rem;
-      margin: 1.25rem 0 1.25rem 0;
-      caret-color: transparent;
-    }
   }
   h3 {
     position: absolute;
     bottom: 5%;
     left: 48vw;
+  }
+  guess-0 {
+    border: red solid 1px;
+    display: inline-block;
+  }
+  .letter {
+    display: inline-block;
+    font-size: 3rem;
+    height: 3rem;
+    width: 3rem;
+    border: 1px solid gray;
+    text-align: center;
   }
 `;
